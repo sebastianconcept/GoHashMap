265f0a2fdcedec1936e8d6789208122c912b71dd
Sebastian Sastre <sebastian.sastre@telna.com>
2022-06-13 17:42:22 -0300

    clear commented code

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/cornelk/hashmap"
 )
 
-// var store = make(map[string]string)
 var defaultSize uintptr = 50
 var storage = hashmap.New(defaultSize)
 
@@ -19,14 +18,12 @@ func reset() {
 
 //export size
 func size() C.uint {
-	// return C.uint(len(store))
 	return C.uint(storage.Len())
 }
 
 //export includes
 func includes(key *C.char) C.bool {
 	var localKey = C.GoString(key)
-	// var _, ok = store[localKey]
 	var _, ok = storage.Get(localKey)
 	return C.bool(ok)
 }
@@ -34,7 +31,6 @@ func includes(key *C.char) C.bool {
 //export get
 func get(key *C.char) *C.char {
 	var localKey = C.GoString(key)
-	// var found, ok = store[localKey]
 	var found, ok = storage.Get(localKey)
 	if ok {
 		return C.CString(found.(string))
@@ -47,7 +43,6 @@ func get(key *C.char) *C.char {
 func set(k *C.char, v *C.char) *C.char {
 	var local_key = C.GoString(k)
 	var local_value = C.GoString(v)
-	// store[local_key] = local_value
 	storage.Set(local_key, local_value)
 	return k
 }
@@ -55,7 +50,6 @@ func set(k *C.char, v *C.char) *C.char {
 //export remove
 func remove(k *C.char) {
 	var local_key = C.GoString(k)
-	// delete(store, local_key)
 	storage.Del(local_key)
 }
 
