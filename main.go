e1e6b3dd058cef3ec564fdd1600c9a05041727be
Sebastian Sastre <sebastian.sastre@telna.com>
2022-06-13 14:41:06 -0300

    with cornel's hashmap

--- main.go
+++ main.go
@@ -1,33 +1,34 @@
 package main
 
 import "C"
+import (
+	"github.com/cornelk/hashmap"
+)
 
-var store = make(map[string]string)
-
-//export getVersion
-func getVersion() *C.char {
-	var version = C.CString("4.3")
-	return version
-}
+// var store = make(map[string]string)
+var storage = &hashmap.HashMap{}
 
 //export size
 func size() C.uint {
-	return C.uint(len(store))
+	// return C.uint(len(store))
+	return C.uint(storage.Len())
 }
 
 //export includes
-// func includes(key *C.char) *C.BOOL {
+// func includes(key *C.char) *C.bool {
 // 	var localKey = C.GoString(key)
-// 	var _, ok = store[localKey]
-// 	return ok
+// 	// var _, ok = store[localKey]
+// 	var _, ok = storage.Get(localKey)
+// 	return C.CBOOL(ok)
 // }
 
 //export get
 func get(key *C.char) *C.char {
 	var localKey = C.GoString(key)
-	var found, ok = store[localKey]
+	// var found, ok = store[localKey]
+	var found, ok = storage.Get(localKey)
 	if ok {
-		return C.CString(found)
+		return C.CString(found.(string))
 	} else {
 		return nil
 	}
@@ -37,14 +38,16 @@ func get(key *C.char) *C.char {
 func set(k *C.char, v *C.char) *C.char {
 	var local_key = C.GoString(k)
 	var local_value = C.GoString(v)
-	store[local_key] = local_value
+	// store[local_key] = local_value
+	storage.Set(local_key, local_value)
 	return k
 }
 
 //export remove
 func remove(k *C.char) {
 	var local_key = C.GoString(k)
-	delete(store, local_key)
+	// delete(store, local_key)
+	storage.Del(local_key)
 }
 
 func main() {
